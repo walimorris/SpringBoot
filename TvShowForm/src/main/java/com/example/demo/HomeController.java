4a81889aa05f6b6476e33c8b5900f2a468e66455
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-08-30 01:44:10 -0700

    Add files via upload

---
+++ TvShowForm/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @GetMapping("/tvshowform")
+    public String loadTvForm(Model model) {
+        model.addAttribute("tvshow", new TvShow());
+        return "tvshowform";
+
+    }
+
+    @PostMapping("/tvshowform")
+    public String processTvForm(@Valid TvShow tvshow, BindingResult result) {
+        if ( result.hasErrors() ) {
+            return "tvshowform"
+        }
+        return "tvshowconfirm";
+    }
+}
